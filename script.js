321a542258639e9d0db537769a8b9d376ae60a16
Saurav Datta <sauravdatta06@gmail.com>
2021-09-13 00:46:44 +0600

    updated

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
-console.log("testing");
+let low = 1;
 
-function getRandomInt(min, max) {
-    console.log(Math.ceil(min));
-    console.log(Math.floor(max));
+let high = 10;
+let chanceLeft = 3;
+
+
+
+
+
+const numberLoop = () => {
+    let correcr_ans = Math.floor(Math.random() * (high - low + 1) + low);
+    let win = false;
+    
+
+
+    for (let i = 1; i <= 3; i++) {
+
+
+        let guessValue = prompt("Enter the number // To Exit Game type 0");
+
+        if(guessValue == 0){
+            win = true;
+            break;
+        }
+
+        if (guessValue == correcr_ans) {
+            alert("You Win !");
+            win = true;
+            break;
+        }
+        else if (guessValue > correcr_ans) {
+           
+            alert("Correct answer is smaller!" + "And you have " + (chanceLeft-i) + " chances left");
+        }
+        else {
+            
+
+            alert("Correct answer is greater! "  + "And you have " + (chanceLeft-i) + " chances left")
+        }
+
+
+    }
+
+    if (win == false ) {
+
+        alert("You Lose!");
+    }
+
+
 }
 
-getRandomInt(parseInt(1),parseInt(10));
+ const restartGame = () =>{
+  numberLoop();
+ }
+
